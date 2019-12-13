253956d0620c21071714cad256f8f77accfca269
DRobinson4105 <58113704+DRobinson4105@users.noreply.github.com>
2019-12-13 06:41:23 -0500

    fixed some of it

--- Chess/libraries/p5.js
+++ Chess/libraries/p5.js
@@ -1,21 +1,18 @@
-/*! p5.js v0.5.8 March 25, 2017 */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.p5 = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 
 },{}],2:[function(_dereq_,module,exports){
-(function (process,global){
-/*!
- * @overview es6-promise - a tiny implementation of Promises/A+.
- * @copyright Copyright (c) 2014 Yehuda Katz, Tom Dale, Stefan Penner and contributors (Conversion to ES6 API by Jake Archibald)
- * @license   Licensed under MIT license
- *            See https://raw.githubusercontent.com/stefanpenner/es6-promise/master/LICENSE
- * @version   4.0.5
- */
 
-(function (global, factory) {
-    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-    typeof define === 'function' && define.amd ? define(factory) :
-    (global.ES6Promise = factory());
-}(this, (function () { 'use strict';
+	(function (process,global){
+	
+	(function (global, factory) {
+		
+		typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+    
+		typeof define === 'function' && define.amd ? define(factory) :
+    
+		(global.ES6Promise = factory());
+
+	}(this, (function () { 'use strict';
 
 function objectOrFunction(x) {
   return typeof x === 'function' || typeof x === 'object' && x !== null;
